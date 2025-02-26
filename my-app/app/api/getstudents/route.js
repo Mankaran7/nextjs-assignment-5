7f58867a640b1e8433efeac371428bef4cfd9c5f
Mankaran Singh <mankaransingh22747@gmail.com>
2025-02-26 16:50:28 +0530

    second-commit

--- my-app/app/api/getstudents/route.js
+++ my-app/app/api/getstudents/route.js
@@ -3,6 +3,6 @@ import data from '../../school.json'
 export function GET(request) {
     const searchParams = request.nextUrl.searchParams
     const id = parseInt(searchParams.get('id'))
-    const arr=data.students.filter((item)=>item.teacher_id!=id)
+    const arr=data.students.filter((item)=>item.teacher_id==id)
     return NextResponse.json({arr})
   }
